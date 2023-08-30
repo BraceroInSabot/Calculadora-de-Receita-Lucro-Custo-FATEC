3edd1afda3d33487f3605e5c70d0de67f578e60e
João Pedro Merlotti <joaopedromerlotti@gmail.com>
2023-08-30 16:57:43 -0300

    Funções funcionando

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import tkinter
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
-from processamento.calculos import equilibrio
+from processamento.calculos import Function, equilibrioDeDuasFuncoes
 
-x = equilibrio(c1=[15, 1400], c2=[5, 2000])[0]
-y1 = equilibrio(c1=[15, 1400], c2=[5, 2000])[1]
-y2 = equilibrio(c1=[15, 1400], c2=[5, 2000])[2]
+x = equilibrioDeDuasFuncoes(caso1=Function(acrescentador=15, fixo=1400), caso2=Function(acrescentador=5, fixo=2000))[0]
+y1 = equilibrioDeDuasFuncoes(caso1=Function(15, 1400), caso2=Function(5, 2000))[1]
+y2 = equilibrioDeDuasFuncoes(caso1=Function(15, 1400), caso2=Function(5, 2000))[2]
+
+print(y1, type(y1), y2, type(y2))
 
 root = tkinter.Tk()
 root.wm_title("Calculator")
 
 figure = Figure(figsize=(5,4), dpi=100)
 ax = figure.add_subplot()
+ax.margins(x=0, y=0, tight=True)
 ax.plot(x, y1, y2)
 
 canvas = FigureCanvasTkAgg(figure, master=root)  # A tk.DrawingArea.
--- processamento/calculos.py
+++ processamento/calculos.py
@@ -1,10 +1,16 @@
-import matplotlib.pyplot as plt
+#calculo = lambda acrescentador, variavel, fixo: acrescentador*variavel + fixo
 
-from typing import List
+class Function():
+    def __init__(self, acrescentador: float, fixo: float):
+        self.a = acrescentador
+        self.b = fixo
 
-calculo = lambda acrescentador, variavel, fixo: acrescentador*variavel + fixo
+    def calculo(self, variavel: float) -> float:
+        x = variavel
+        posY = self.a*x + self.b
+        return posY
 
-def equilibrio(c1: List[float], c2: List[float]) -> object:
+def equilibrioDeDuasFuncoes(caso1: Function, caso2: Function) -> object:
     """
     Realiza calculos até achar o ponto de equilibro entre duas funções semelhantes
     de primeiro grau.
@@ -14,27 +20,15 @@ def equilibrio(c1: List[float], c2: List[float]) -> object:
     c2 -> Lista com dois itens contendo: valor acrescentador e valor fixo
     """
     contador: int = 0
-    valor1, valor2 = [c1[1]], [c2[1]]
-    eixoX: List[float] = [0]
+    valor1, valor2 = [caso1.calculo(0)], [caso2.calculo(0)]
+    eixoX = [0]
 
     while (contador != 100):
         contador+=1
         eixoX.append(contador)
-        valor1.append(
-        calculo(
-            acrescentador=c1[0],
-            variavel=contador,
-            fixo=c1[1]
-            )
-        )
-        valor2.append(
-        calculo(
-            acrescentador=c2[0],
-            variavel=contador,
-            fixo=c2[1]
-            )
-        )
+        valor1.append(float(caso1.calculo(variavel=contador)))
+        valor2.append(float(caso2.calculo(variavel=contador)))
 
-        return [eixoX, valor1, valor2]
+    return (eixoX, valor1, valor2)
 
 
